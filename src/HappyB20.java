fd4e7e0f3e6e8c1d42f3fecdeec4d83b00a3a300
FatihDemircan <fdfatihdemircan@gmail.com>
2020-08-01 11:50:57 -0400

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,5 +1,9 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
 public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("B20 is a happy class.");
+        System.out.println("I love git course");
     }
 }
